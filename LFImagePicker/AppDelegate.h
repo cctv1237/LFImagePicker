b44b7bbf47cc9e64ec5c55ae9a2ef49638c2d2b7
LF <marklf@163.com>
2015-09-25 12:02:19 +0800

    Initial Commit

---
+++ LFImagePicker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LFImagePicker
+//
+//  Created by LongFan on 15/9/24.
+//  Copyright © 2015年 LongFan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LFImagePicker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LFImagePicker
+//
+//  Created by LongFan on 15/9/24.
+//  Copyright © 2015年 LongFan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
