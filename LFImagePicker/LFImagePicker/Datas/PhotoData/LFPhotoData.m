dcda65d7525b216882de2014fb55cecd99f3dd14
LongFan <marklf@163.com>
2015-12-02 16:44:49 +0800

    fixed null album bug

--- LFImagePicker/LFImagePicker/Datas/PhotoData/LFPhotoData.m
+++ LFImagePicker/LFImagePicker/Datas/PhotoData/LFPhotoData.m
@@ -38,7 +38,11 @@
 - (void)configAlbumByAlbums:(PHFetchResult *)albums
 {
     [albums enumerateObjectsUsingBlock:^(PHAssetCollection * _Nonnull collection, NSUInteger idx, BOOL * _Nonnull stop) {
-        if ([collection.localizedTitle isEqualToString:@"Camera Roll"]||[collection.localizedTitle isEqualToString:@"相机胶卷"]) {
+        if ([collection.localizedTitle isEqualToString:@"Camera Roll"]
+            ||[collection.localizedTitle isEqualToString:@"相机胶卷"]
+            ||[collection.localizedTitle isEqualToString:@"All Photos"]
+            ||[collection.localizedTitle isEqualToString:@"所有照片"]) {
+            
             self.album = collection;
             PHFetchResult *assetsFetchResult = [PHAsset fetchAssetsInAssetCollection:collection options:nil];
             [self configPhotosByAlbum:assetsFetchResult];
